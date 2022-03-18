6477189336f424f6dc36416c8408b53a1f8a09d4
hark130 <hark130@yahoo.com>
2022-03-18 17:09:00 -0500

    code: BadCode initial commit
    How do you verify test code works?
    By testing the test code, of course!
    BadCode is a basic Python package we can use to test how TEST tests
    code.
    
    Changes to be committed:
    	new file:   badcode/__init__.py
    	new file:   badcode/__main__.py
    	new file:   badcode/main.py
    	new file:   badcode/maths.py
    	new file:   badcode/parser.py

---
+++ badcode/maths.py
@@ -0,0 +1,12 @@
+"""Do Bad Code maths."""
+
+
+def divide_it(numerator: int, denominator: int) -> float:
+    """Find the quotient."""
+    if not isinstance(numerator, int):
+        raise TypeError('numerator must be an int')
+    if not isinstance(denominator, int):
+        raise TypeError('denominator must be an int')
+    if denominator == 0:
+    	raise ValueError('You may not divide by zero')
+    return numerator / denominator
