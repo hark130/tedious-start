f0cac6768428e2b761396c8916295437848aa437
hark130 <hark130@yahoo.com>
2023-05-11 16:54:11 -0500

    TEST-23: Made Pycodestyle happy
    Changes to be committed:
    	modified:   test/__init__.py

--- test/__init__.py
+++ test/__init__.py
@@ -1,4 +1,4 @@
 """Defines package-wide variables for the TEDIOUS START (TEST) testing."""
 # SPOT for the environment variable name used by this project's scripts to set test case verbosity.
-TEST_VERB_LEVELS = [0,1,2]                  # 0 is quiet, 1 is default, and 2 is verbose
+TEST_VERB_LEVELS = [0, 1, 2]                # 0 is quiet, 1 is default, and 2 is verbose
 TEST_ENV_VAR_NAME = 'TEST_VERBOSITY_LEVEL'  # Environment variable to test for verbosity
