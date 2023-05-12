147ce3948e82990cc7781b076cc3c815179945b5
hark130 <hark130@yahoo.com>
2023-05-12 13:58:04 -0500

    TEST-23: Refactored test_local.sh
    I needed to extricate functionality to avoid duplication.
    I discovered this when I began implementing the "test installation"
    script.
    
    Changes to be committed:
    	modified:   devops/scripts/test_local.sh
    
    Untracked files:
    	devops/scripts/test_installation.sh
    	devops/scripts/test_local_examples.sh

--- devops/scripts/test_local.sh
+++ devops/scripts/test_local.sh
@@ -1,67 +1,53 @@
 #!/bin/bash
 
+# This script automates the execution of the TEDIOUS START (TEST) example code and TEST test
+# code: functional tests, unit tests.  The verbosity of these tests is controlled by the
+# TEST_VERBOSITY_LEVEL environment variable.  This script controls the value of that environment
+# variable with the UNITTEST_VERBOSITY variable.
 # 
 # This script uses the following exit codes:
 #   0 on success
 #   1 if any Python command exits with a non-zero value
 
 # GLOBAL VARIABLES
-UNITTEST_VERBOSITY=0       # Verbosity level for the test cases to execute with
 # Supported Verbosity Values
 # 0 (quiet): Prints the total numbers of tests executed and the global result.
 # 1 (standard): Same output as quiet with single characters (dot or F) for test cases.
 # 2 (verbose): Prints the help string of every test and the result.
-EXIT_CODE=0                # Exit code used by this script
-ORIGINAL_DIRECTORY=$(pwd)  # Starting directory for this script
+UNITTEST_VERBOSITY=0                                                # Test case verbosity level
+EXIT_CODE=0                                                         # Exit code used by this script
+ORIGINAL_DIR=$(pwd)                                                 # Working dir of execution
+SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"  # This scripts directory
+
 
 # SETUP
 export TEST_VERBOSITY_LEVEL=$UNITTEST_VERBOSITY
 
-echo "TEST EXAMPLE USAGE"
-# TediousStart Examples
-python3 -m test.example_test_start
+
+# TEST IT
+# TEDIOUS START (TEST) Examples
+$SCRIPT_DIR/test_local_examples.sh
 if [ $? -eq 0 ]
 then
-    echo -e "[✓] Example TediousStart test cases\n"
+    echo -e "[✓] TEDIOUS START (TEST) example test cases\n"
 else
-    echo -e "[X] Example TediousStart test cases\n"
+    echo -e "[X] TEDIOUS START (TEST) example test cases\n"
     EXIT_CODE=1
 fi
-
-# TediousUnitTest Examples
-python3 -m test.unit_tests.example_test_unittest
-if [ $? -eq 0 ]
-then
-    echo -e "[✓] Example TediousUnitTest test cases\n"
-else
-    echo -e "[X] Example TediousUnitTest test cases\n"
-    EXIT_CODE=1
-fi
-
-# TediousFuncTest Examples
-python3 -m test.functional_tests.example_test_functest
-if [ $? -eq 0 ]
-then
-    echo -e "[✓] Example TediousFuncTest test cases\n"
-else
-    echo -e "[X] Example TediousFuncTest test cases\n"
-    EXIT_CODE=1
-fi
-
 
 echo "TEDIOUS START TEST CASES"
-python3 -m unittest
+python3 -m test
 if [ $? -eq 0 ]
 then
-    echo -e "[✓] TEDIOUS START (TEST) test cases\n"
+    echo -e "[✓] TEDIOUS START test cases\n"
 else
-    echo -e "[X] TEDIOUS START (TEST) test cases\n"
+    echo -e "[X] TEDIOUS START test cases\n"
     EXIT_CODE=1
 fi
 
 
 # DONE
-cd $ORIGINAL_DIRECTORY
+cd $ORIGINAL_DIR
 unset TEST_VERBOSITY_LEVEL
 echo ""
 exit $EXIT_CODE
